87b9cd4b52e366d6fb46dd6d514cd50e949188ea
oxidsimongassenschmidt <simon.gassenschmidt@oxid-esales.com>
2021-11-10 15:32:42 +0100

    202111101532

--- metadata.php
+++ metadata.php
@@ -7,23 +7,23 @@ $sMetadataVersion = '2.1';
 /**
  * Module information
  */
-$aModule = array(
+$aModule = [
     'id'           => 'oxspicturecleanup',
     'title'        => [
         'de' => 'OXID Support Entferne Duplikate-Bilder mit gleichen Namen',
         'en' => 'OXID Support Remove Duplicate Picture with same names',
     ],
-    'description'  => array(
+    'description'  => [
         'en' => 'Deletes old picture if a new upload happens with same picture name, so no duplicates remain',
         'de' => 'Löscht alte Bilder falls ein neuer Upload mit dem gleichen Bildernamen passiert, so dass keine Duplikate verbleiben',
-    ),
+    ],
     'thumbnail'    => 'example.jpg',
     'version'      => '1.0.0',
     'author'       => 'OXID Support',
     'url'          => 'http://www.oxid-esales.com',
     'email'        => 'support@oxid-esales.com',
-    'extend'       => array(
+    'extend'       => [
         OxidEsales\Eshop\Application\Controller\Admin\ArticlePictures::class  => OxidSupport\PictureCleanup\Controllers\Admin\ArticlePicture::class,
-    ),
+    ],
 
-);
+];
