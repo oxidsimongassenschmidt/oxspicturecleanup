9e87a1fcb8d9a4d0cf42fa49d59dd06b9b17f5d8
oxidsimongassenschmidt <simon.gassenschmidt@oxid-esales.com>
2021-11-10 12:54:42 +0100

    202111101254

--- Controllers/Admin/ArticlePicture.php
+++ Controllers/Admin/ArticlePicture.php
@@ -13,7 +13,7 @@ class ArticlePicture extends ArticlePicture_parent
      */
     public function save()
     {
-        /** @var oxarticle $oArticle */
+        /** @var \OxidEsales\Eshop\Application\Model\Article $oArticle */
         $oArticle = oxNew(Article::class);
         if ($oArticle->load($this->getEditObjectId())) {
             $this->removeOldMasterPictureIfANewPictureIsReplacingIt($oArticle);
@@ -23,17 +23,17 @@ class ArticlePicture extends ArticlePicture_parent
 
     /**
      * Removes old master picture by the new one.
-     * @param oxArticle $article
+     * @param \OxidEsales\Eshop\Application\Model\Article $article
      */
     protected function removeOldMasterPictureIfANewPictureIsReplacingIt(\OxidEsales\Eshop\Application\Model\Article $article)
     {
-        /** @var oxPictureHandler $oxPicHandler */
-        $oxPicHandler = oxNew(PictureHandler::class);
+        /** @var oxPictureHandler $pictureHandler */
+        $pictureHandler  = oxNew(PictureHandler::class);
         foreach ($_FILES['myfile']['name'] as $key => $value) {
             if (!empty($value)) {
                 preg_match('/.*[^\d]([\d]+)/', $key, $matches);
                 if (count($matches) === 2) {
-                    $oxPicHandler->deleteArticleMasterPicture($article, $matches[1]);
+                    $pictureHandler->deleteArticleMasterPicture($article, $matches[1]);
                 }
             }
         }
--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 
 The module prevents that when uploading an image with the same name, a copy of the old image remains on the server. This will now be deleted.
 
-
 The module was developed to save server space.
 
 
 ## Install
-
-1. composer require oxid-support/picturecleanup
-
-2. activate modul
-
+1. install per composer:
+```
+composer require oxid-support/picturecleanup
+```
+2. activate modul per
+a.) console-command:
+```
+vendor/bin/oe-console oe:module:activate oxspicturecleanup
+```
+b.) in backend of shop
 
 
